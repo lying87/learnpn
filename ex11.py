eb157678fa6072e9312bc1a3e700a4a61a4b28c9
lying87 <604065312@qq.com>
2016-12-19 21:04:42 +0800

    ex10.py

---
+++ ex11.py
@@ -0,0 +1,9 @@
+print "How old are you?",
+age = raw_input (38)
+print "How tall are you?",
+height = raw_input ("6'\2")
+print "How much do you weigh?",
+weihgt = raw_input ("1301bs")
+ 
+print "So,you're %r old ,%r tall and %r heavy."% (
+      38 , "6'\2" ,"1301bs" ) 
